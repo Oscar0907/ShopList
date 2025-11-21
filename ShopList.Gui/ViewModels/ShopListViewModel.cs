221b99e703cccc30d57c0559d42443511c961d09
Oscar Alejandro Lopez Kuc <24470032@campeche.tecnm.mx>
2025-11-21 11:12:39 -0600

    Utiliza MVVM con CommunityToolkit.Mvvm

--- ShopList.Gui/ViewModels/ShopListViewModel.cs
+++ ShopList.Gui/ViewModels/ShopListViewModel.cs
@@ -1,57 +1,62 @@
-﻿using ShopList.Gui.Model;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using ShopList.Gui.Model;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Windows.Input;
+//using System.ComponentModel;
+//using System.Windows.Input;
 
 namespace ShopList.Gui.ViewModels
 {
-    public class ShopListViewModel : INotifyPropertyChanged
+    public partial class ShopListViewModel : ObservableObject
     {
+        [ObservableProperty]
         private string _nombreDelArticulo = string.Empty;
+        [ObservableProperty]
         private int _cantidadAComprar = 1;
 
-        public event PropertyChangedEventHandler? PropertyChanged;
+        //public event PropertyChangedEventHandler? PropertyChanged;
 
         public ObservableCollection<Item> Items { get; }
 
-        public string NombreDelArticulo
-        {
-            get => _nombreDelArticulo;
-            set
-            {
-                if(value != _nombreDelArticulo)
-                {
-                    _nombreDelArticulo = value;
-                    OnPropertyChanged(nameof(NombreDelArticulo));
-                }
-            }
-        }
-        public int CantidadAComprar
-        {
-            get => _cantidadAComprar;
-            set
-            {
-                if (value != _cantidadAComprar)
-                {
-                    _cantidadAComprar = value;
-                    OnPropertyChanged(nameof(CantidadAComprar));
-                }
-            }
-        }
+        //public string NombreDelArticulo
+        //{
+        //    get => _nombreDelArticulo;
+        //    set
+        //    {
+        //        if(value != _nombreDelArticulo)
+        //        {
+        //            _nombreDelArticulo = value;
+        //            OnPropertyChanged(nameof(NombreDelArticulo));
+        //        }
+        //    }
+        //}
+        //public int CantidadAComprar
+        //{
+        //    get => _cantidadAComprar;
+        //    set
+        //    {
+        //        if (value != _cantidadAComprar)
+        //        {
+        //            _cantidadAComprar = value;
+        //            OnPropertyChanged(nameof(CantidadAComprar));
+        //        }
+        //    }
+        //}
 
-        public ICommand AgregarShopListItemCommand 
-        { 
-            get; 
-            private set; 
-        }
+        //public ICommand AgregarShopListItemCommand 
+        //{ 
+        //    get; 
+        //    private set; 
+        //}
 
         public ShopListViewModel()
         {
             Items = new ObservableCollection<Item>();
             CargarDatos();
-            AgregarShopListItemCommand = new Command(AgregarShopListItem);
+            //AgregarShopListItemCommand = new Command(AgregarShopListItem);
         }
 
+        [RelayCommand]
         public void AgregarShopListItem()
         {
             if(string.IsNullOrEmpty(NombreDelArticulo) || CantidadAComprar <= 0)
@@ -70,6 +75,7 @@ namespace ShopList.Gui.ViewModels
             NombreDelArticulo = string.Empty;
             CantidadAComprar = 1;
         }
+        [RelayCommand]
         public void EliminarShopListItem()
         {
 
@@ -101,9 +107,9 @@ namespace ShopList.Gui.ViewModels
 
             });
         }
-        private void OnPropertyChanged(string propertyName) 
-        {
-            PropertyChanged?.Invoke (this, new PropertyChangedEventArgs(propertyName));
-        }
+        //private void OnPropertyChanged(string propertyName) 
+        //{
+        //    PropertyChanged?.Invoke (this, new PropertyChangedEventArgs(propertyName));
+        //}
     }
 }
